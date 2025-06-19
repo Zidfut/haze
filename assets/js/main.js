b1889a29dbc8c796e7da531cf7c3bb65a8256714
zidfut <zidz.ua@gmail.com>
2025-06-20 01:33:21 +0300

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,10 +36,10 @@
   }
 })();
 new Swiper("[data-rewiew-slider-js]", {
-  loop: true,
+  // loop: true,
   slidesPerView: "auto",
   spaceBetween: 32,
-  loopAdditionalSlides: 1,
+  // loopAdditionalSlides: 1,
   centeredSlides: true,
   grabCursor: true,
   pagination: {
@@ -48,5 +48,22 @@ new Swiper("[data-rewiew-slider-js]", {
   navigation: {
     nextEl: ".reviews__slider-button-next",
     prevEl: ".reviews__slider-button-prev"
+  },
+  breakpoints: {
+    768: {
+      centeredSlides: false
+    }
+  }
+});
+new Swiper("[data-ugc-slider-js]", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 32,
+  loopAdditionalSlides: 2,
+  centeredSlides: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".ugc__slider-button-next",
+    prevEl: ".ugc__slider-button-prev"
   }
 });
