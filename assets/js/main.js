fe55b3ebacdec758619cc8f54eaf0a932c87f6f7
zidfut <zidz.ua@gmail.com>
2025-06-19 17:44:39 +0300

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,18 @@
     fetch(link.href, fetchOpts);
   }
 })();
+new Swiper("[data-rewiew-slider-js]", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 32,
+  loopAdditionalSlides: 1,
+  centeredSlides: true,
+  grabCursor: true,
+  pagination: {
+    el: ".reviews__slider-pagination"
+  },
+  navigation: {
+    nextEl: ".reviews__slider-button-next",
+    prevEl: ".reviews__slider-button-prev"
+  }
+});
