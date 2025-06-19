a471c8b6b7c30c6afb24565265caffcbe0c82f28
zidfut <zidz.ua@gmail.com>
2025-06-20 02:01:43 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
-const swiper = new Swiper("[data-rewiew-slider-js]", {
-  loop: true,
+const rewiewSlider = new Swiper("[data-rewiew-slider-js]", {
+  // loop: true,
   slidesPerView: "auto",
   spaceBetween: 32,
-  loopAdditionalSlides: 1,
+  // loopAdditionalSlides: 1,
   centeredSlides: true,
   grabCursor: true,
   pagination: {
@@ -11,5 +11,23 @@ const swiper = new Swiper("[data-rewiew-slider-js]", {
   navigation: {
     nextEl: ".reviews__slider-button-next",
     prevEl: ".reviews__slider-button-prev",
+  },
+  breakpoints: {
+    768: {
+      centeredSlides: false,
+    },
+  }
+});
+
+const ugcSlider = new Swiper("[data-ugc-slider-js]", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 32,
+  loopAdditionalSlides: 2,
+  centeredSlides: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".ugc__slider-button-next",
+    prevEl: ".ugc__slider-button-prev",
   },
 });
