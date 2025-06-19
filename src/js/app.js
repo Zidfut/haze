ba1dfdedbb7a6549a03852a4e0d551b606b7441e
zidfut <zidz.ua@gmail.com>
2025-06-19 17:43:21 +0300

    added reviews slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
-// Custom Scripts
-
-// document.body.hidden = true // test
-
+const swiper = new Swiper("[data-rewiew-slider-js]", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 32,
+  loopAdditionalSlides: 1,
+  centeredSlides: true,
+  grabCursor: true,
+  pagination: {
+    el: ".reviews__slider-pagination",
+  },
+  navigation: {
+    nextEl: ".reviews__slider-button-next",
+    prevEl: ".reviews__slider-button-prev",
+  },
+});
