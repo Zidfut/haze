7732e530eabb3ffdaeabd0bd734750d6192df470
zidfut <zidz.ua@gmail.com>
2025-06-24 21:07:37 +0300

    added faq section

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ const rewiewSlider = new Swiper("[data-rewiew-slider-js]", {
     768: {
       centeredSlides: false,
     },
-  }
+  },
 });
 
 const ugcSlider = new Swiper("[data-ugc-slider-js]", {
@@ -30,4 +30,47 @@ const ugcSlider = new Swiper("[data-ugc-slider-js]", {
     nextEl: ".ugc__slider-button-next",
     prevEl: ".ugc__slider-button-prev",
   },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".faq__accordion-item-header").forEach((header) => {
+    header.addEventListener("click", () => {
+      const item = header.closest(".faq__accordion-item");
+
+      const isActive = item.classList.contains("active");
+
+      document
+        .querySelectorAll(".faq__accordion-item.active")
+        .forEach((activeItem) => {
+          activeItem.classList.remove("active");
+        });
+
+      if (!isActive) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  const filterLinks = document.querySelectorAll(".faq__accordion-filters a");
+  const items = document.querySelectorAll(".faq__accordion-item");
+
+  filterLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      filterLinks.forEach((l) => l.classList.remove("active"));
+      link.classList.add("active");
+
+      const filter = link.dataset.filter;
+
+      items.forEach((item) => {
+        const category = item.dataset.category;
+        if (filter === "All" || category === filter) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+  });
 });
